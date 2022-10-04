5c5cadf7285f6b545ef7529a953f392408714ec6
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-10-04 14:05:13 +0300

    lab completed!!

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,5 +1,6 @@
 class ToysController < ApplicationController
   wrap_parameters format: []
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     toys = Toy.all
@@ -7,25 +8,39 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy
+  end
+
+  def increment_likes
+    toy = find_toy
+    toy.update(likes: toy.likes + 1)
+    render json: toy
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
   end
 
   private
   
+  def find_toy
+    Toy.find(params[:id])
+  end
+
   def toy_params
     params.permit(:name, :image, :likes)
   end
 
+  def render_not_found_response
+    render json: { error: "Toy not found" }, status: :not_found
+  end
 end
